c7d1dc92ade70bea116ef642f6276a4d81113dd5
GravyHTX <godisgravy@gmail.com>
2020-03-26 21:27:38 -0500

    Heroku try

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
@@ -49,6 +49,6 @@ db.sequelize.sync(syncOptions).then(function() {
       PORT
     );
   });
-});
+// });
 
 module.exports = app;
